05c3d7a7e96b805c8fa4ebd957ce1faebb037486
xiaotianlyu <xiaotianlyu@outlook.com>
2023-09-15 20:11:38 -0400

    add functions

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void printLine(String s) {
+        System.out.println(s);
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
 }
